b8972ee3da4ec924047708aef9cb275067c8efb6
Anna9eOnufrijeva1 <122529564+Anna9eOnufrijeva1@users.noreply.github.com>
2023-01-26 14:34:11 +0200

    Klases darbs

---
+++ Source.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+//int main() {
+//	int a = 1;
+//	do {
+//		int b = 1;
+//		do {
+//			cout << a << endl;
+//			b = b + 1;
+//		} while (b <= 5);
+//		a = a + 1;
+//	} while (a <= 3);
+//	return 0;
+//}
+
+//int main() {
+//	int x = 10;
+//	switch (x)
+//	{
+//	case 10:
+//		if (x == 10) { cout << "X is 10" << endl; }
+//	case 20:
+//		if (x == 20) { cout << "X is 20" << endl; }
+//	case 30:
+//		if (x == 30) { cout << "X is 30" << endl; }
+//	default:
+//		if (x != 10 and x != 20 and x != 30) { cout << "X is not 10, 20 or 30" << endl; }
+//	}
+//	return 0;
+//}
+
+//int main() {
+//	int a = 1;
+//	int b = 2;
+//	cout << a << endl << b << endl;
+//	{
+//		if (a < 0 or b < 0) { cout << "Viens no skaitliem  ir negativs" << endl; }
+//		else cout << "Neviens no skaitliem nav negativs" << endl;
+//}
+//	{
+//		if (a > b) { cout << "a ir lielaks par b" << endl; }
+//		else cout << "a ir mazaks par b" << endl;
+//}
+//	{
+//		if (a % 2 == 0) { cout << "a ir para skaitlis" << endl; }
+//		else cout << "a ir nepara sakitlis" << endl;
+//		if (b % 2 == 0) { cout << "b ir para skaitlis" << endl; }
+//		else cout << "b ir nepara skaitlis" << endl;
+//}
+//}
+
+//int main() {
+//	int p = 80;
+//	{
+//		if (p >= 90) { cout << "Grade A" << endl; }
+//		if (p >= 80&&p < 90) { cout << "Grade B" << endl; }
+//		if (p >= 70&&p < 80) { cout << "Grade C" << endl; }
+//		if (p >= 60&&p < 70) { cout << "Grade D" << endl; }
+//		if (p >= 40&& p < 60) { cout << "Grade E" << endl; }
+//		else cout << "Grade F" << endl; 
+//	}
+//	return 0;
+//}
